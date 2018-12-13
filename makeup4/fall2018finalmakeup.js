0833c747a1e84861b76024df9360542f66993eb3
Jenna Yun (RIT Student) <sy8846@ad.rit.edu>
2018-12-13 11:10:57 -0500

    test

---
+++ makeup4/fall2018finalmakeup.js
@@ -0,0 +1,5 @@
+/*Jquery*/
+$(".menu").click(function() {  /*Whenever you click the menubox class*/
+    $(this).find(".submenu").toggle(); /*find is searching a child*/
+});/*whichever menu you click, it will show or hide*/
+
